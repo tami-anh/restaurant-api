7d9347f6ffb7461e5e267ce292c4322f5f576dd0
tami-anh <tami.kyanh@gmail.com>
2019-12-10 19:57:16 +0100

    I added a comment

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        //This is comment
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
